4b71dce6b9c1236100e84dcf4499a0fc3e9d0f11
Fredy Cea <fceaurrutia@gmail.com>
2022-05-03 00:01:25 -0400

    Added base components and base functionallity

--- src/Views/Main.tsx
+++ src/Views/Main.tsx
@@ -1,29 +1,10 @@
 import React from "react";
-import { useAppDispatch, useAppSelector } from "../Redux/Hooks";
-import {
-	getPosts,
-	selectPosts,
-	getPostsWithoutFilter,
-} from "../Redux/Reducers/PostSlice";
+import Table from "../Components/Table";
 
 function Main() {
-	const dispatch = useAppDispatch();
-	const { posts } = useAppSelector((state) => state.posts);
-	const dispatchPosts = () => {
-		dispatch(getPostsWithoutFilter());
-	};
-	console.log(posts);
 	return (
-		<div className="App">
-			Main
-			<button
-				onClick={() => {
-					console.log("Pressed");
-					dispatchPosts();
-				}}
-				type="button">
-				Botón
-			</button>
+		<div>
+			<Table />
 		</div>
 	);
 }
