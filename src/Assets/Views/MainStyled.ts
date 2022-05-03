1fbc4309d7baf32e260c3cf132fad6110d5d2c16
Fredy Cea <fceaurrutia@gmail.com>
2022-05-03 01:25:44 -0400

    Finished General layout on Main component, only table and item left

---
+++ src/Assets/Views/MainStyled.ts
@@ -0,0 +1,133 @@
+import { styled, Select } from "@mui/material";
+
+export const Title = styled("div")(({ theme }) => ({
+	display: "flex",
+	boxShadow: "0 1px 4px 0 rgba(0, 21, 41, 0.12)",
+	[theme.breakpoints.up("md")]: {
+		padding: "0 98px",
+	},
+	[theme.breakpoints.down("md")]: {
+		justifyContent: "center",
+	},
+	background:
+		"linear-gradient(180deg, rgba(236,236,236,1) -32%, rgba(255,255,255,1) 124%)",
+	"& > h3": {
+		objectFit: "contain",
+		fontFamily: "Baskerville",
+		fontSize: "28px",
+		fontWeight: "normal",
+		fontStretch: "normal",
+		fontStyle: "normal",
+		lineHeight: 1,
+		letterSpacing: "normal",
+		color: "#3b3b3b",
+	},
+}));
+
+export const TableContainer = styled("div")(({ theme }) => ({
+	display: "flex",
+	marginTop: "20px",
+	[theme.breakpoints.up("md")]: {
+		padding: "0 98px",
+	},
+	[theme.breakpoints.down("md")]: {
+		padding: "0 20px",
+	},
+}));
+
+export const SelectorContainer = styled("div")({
+	marginTop: 50,
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	height: 24,
+});
+
+export const FilterContainer = styled("div")(({ theme }) => ({
+	display: "flex",
+	justifyContent: "space-between",
+	marginTop: 20,
+	[theme.breakpoints.up("md")]: {
+		padding: "0 98px",
+	},
+	[theme.breakpoints.down("md")]: {
+		padding: "0 20px",
+	},
+}));
+
+export const Mode = styled("div")({
+	display: "flex",
+	gridGap: 10,
+	justifyContent: "center",
+	alignItems: "center",
+	textAlign: "center",
+});
+
+export const IconButton = styled("div")({
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	textAlign: "center",
+	cursor: "pointer",
+	transition: "0.125s all ease-in",
+	borderRadius: "50%",
+	padding: 10,
+	border: "1px solid rgba(0, 0, 0, .1)",
+	"&:hover": {
+		backgroundColor: "rgba(225, 225, 225, 0.5)",
+	},
+});
+
+export const Selection = styled("div")<{ active: boolean }>(
+	({ theme, active }) => ({
+		userSelect: "none",
+		cursor: "pointer",
+		[theme.breakpoints.only("xs")]: {
+			width: "33%",
+		},
+		[theme.breakpoints.up("sm")]: {
+			width: "25%",
+		},
+		[theme.breakpoints.up("md")]: {
+			width: "10%",
+		},
+		[theme.breakpoints.up("lg")]: {
+			width: "8%",
+		},
+		height: "100%",
+		borderRadius: "2px",
+		border: active ? "solid 1px #1797ff" : "solid 1px #d6d6d6",
+		"&:hover": {
+			backgroundColor: "rgba(225, 225, 225, 0.5)",
+		},
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		transition: "0.25s all ease-in",
+		"& > p": {
+			fontSize: 16,
+			fontFamily: "Roboto",
+			fontWeight: 500,
+			lineHeight: 1.75,
+			fontStretch: "normal",
+			fontStyle: "normal",
+			textAlign: "center",
+			color: "#1797ff",
+		},
+	}),
+);
+
+export const StyledSelect = styled(Select)(({ theme }) => ({
+	[theme.breakpoints.only("xs")]: {
+		width: "40%",
+	},
+	[theme.breakpoints.up("sm")]: {
+		width: "25%",
+	},
+	[theme.breakpoints.up("md")]: {
+		width: "15%",
+	},
+	[theme.breakpoints.up("lg")]: {
+		width: "15%",
+	},
+}));
--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -10,23 +10,7 @@ import {
 function Table() {
 	const [currentPage, setCurrentPage] = useState<number>(0);
 	const query = localStorage.getItem("query");
-	const options = [
-		{
-			value: "angular",
-			label: "Angular",
-			icon: "NoIcon",
-		},
-		{
-			value: "reactjs",
-			label: "React",
-			icon: "NoIcon",
-		},
-		{
-			value: "vuejs",
-			label: "Vue",
-			icon: "NoIcon",
-		},
-	];
+
 	const dispatch = useAppDispatch();
 	const { posts } = useAppSelector((state) => state.posts);
 	const dispatchPosts = () => {
@@ -37,37 +21,15 @@ function Table() {
 		);
 	};
 
-	const handleChangeSelectFilter = (
-		e: React.ChangeEvent<HTMLSelectElement>,
-	) => {
-		const {
-			target: { value },
-		} = e;
-		localStorage.setItem("query", value);
-	};
-
 	useEffect(() => {
 		dispatchPosts();
 	}, []);
 
 	return (
-		<div>
-			<div>
-				<label htmlFor="filterSelect">
-					<select id="filterSelect" onChange={handleChangeSelectFilter}>
-						{options.map((x) => (
-							<option key={x.value} value={x.value}>
-								{x.label}
-							</option>
-						))}
-					</select>
-				</label>
-			</div>
-			<div className="table-container">
-				{posts.map((x) => (
-					<Item post={x} />
-				))}
-			</div>
+		<div className="table-container">
+			{posts.map((x) => (
+				<Item post={x} />
+			))}
 		</div>
 	);
 }
